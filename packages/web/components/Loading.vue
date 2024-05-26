cb5aec7069f96e1ae8254e259a184202b4e0193b
CofCat456 <a25690756@gmail.com>
2024-05-26 20:53:22 +0800

    perf(web): remove unused v-if

--- packages/web/components/Loading.vue
+++ packages/web/components/Loading.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div v-if="true" flex="~ col" h-full w-full items-center justify-center p4>
+  <div flex="~ col" h-full w-full items-center justify-center p4>
     <div flex="~ gap-2 items-center" font-mono flex-auto animate-pulse text-xl>
       <div i-svg-spinners-90-ring-with-bg />
       Loading data...
